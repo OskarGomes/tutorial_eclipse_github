41eaae300a89894db97b11b3346dcd90b642f62f
Oskar Gomes <ogomes001>
2016-11-12 11:57:17 +0100

    Es el primer commit de prueba

---
+++ ProyectoPrueba/src/org/inv/packPrueba/Persona.java
@@ -0,0 +1,35 @@
+package org.inv.packPrueba;
+
+public class Persona {
+
+	private String nombre;
+	private int edad;
+	private boolean puedeConducir;
+	
+	public Persona(String pNom,int pEdad){
+		this.nombre = pNom;
+		if(pEdad<18){
+			this.puedeConducir=false;
+		}
+		else{
+			this.puedeConducir=true;
+		}
+	}
+	
+	public void puedeConducir(){
+		
+		String puede;
+		if(puedeConducir){
+			puede= "puede";
+		}
+		else{
+			puede= "no puede";
+		}
+	
+		System.out.println("Esta persona llamada "+this.nombre+" "+ puede+" conducir en España.");
+	}
+	
+	public boolean conduce(){
+		return puedeConducir;
+	}
+}
